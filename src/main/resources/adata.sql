cc1ee26f92a1ee2bd968de09624cd939cc48d597
Th-Sanchez <thibpeluci@gmail.com>
2026-09-24 07:37:10 -0300

    chore: remove unused SQL and endpoint test documentation

--- src/main/resources/adata.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO usuarios (id, ativo, created_at, updated_at, nome, email, password_hash, role, tel1, tel2)
-VALUES (random_bytes(16), true, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 'Admin', 'admin',
-        '$2a$10$8lwarDgK2sqiKAJKIR8vD.a0XtUYURmaUT5oHx4xwAUQn2NT09qNm', 'ADMIN', '00000000000', NULL);
